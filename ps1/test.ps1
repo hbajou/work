629fe4dc4b14a93f13ef564c61eefcf429441a82
hbajou <dinner.draggy9y@icloud.com>
2025-08-14 18:52:05 +0900

    first commit

---
+++ ps1/test.ps1
@@ -0,0 +1,13 @@
+﻿$csvData = Import-Csv ".\input.csv"
+
+foreach ($row in $csvData) {
+    $action = $row.PSObject.Properties["action"].Value
+    $user   = $row.PSObject.Properties["user"].Value
+
+    switch ($action) {
+        "ユーザ作成" { Write-Host "【作成】$user を登録します。" }
+        "ユーザ削除" { Write-Host "【削除】$user を削除します。" }
+        "グループ追加"  { Write-Host "【確認】$user の情報を表示します。" }
+        default  { Write-Host "【不明】$user に対する操作 '$action' は未対応です。" }
+    }
+}
